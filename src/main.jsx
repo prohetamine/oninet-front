259c55f881ea15a02211c91fd24ba616d14c0b0b
Stas Prohetamine <prohetamine@gmail.com>
2025-06-08 06:32:19 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,13 @@ const initializeTelegramSDK = async () => {
   try {
     await init()
 
-    postEvent('web_app_expand')
     postEvent('web_app_setup_main_button', { is_visible: false })
     postEvent('web_app_setup_swipe_behavior', { allow_vertical_swipe: false })
     postEvent('web_app_set_background_color', { color: '#15B585' })
     postEvent('web_app_set_bottom_bar_color', { color: '#15B585' })
     postEvent('web_app_set_header_color', { color: '#0EA795' });
-    
+    postEvent('web_app_expand')
+
     if (miniApp.ready.isAvailable()) {
       await miniApp.ready()
     }
